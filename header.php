1612643e8b1ab97c66a13a445f92a81529d9d36c
yamato0911 <60542163+yamato0911@users.noreply.github.com>
2020-06-19 20:28:43 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Copy</title>
+    <link href="https://unpkg.com/sanitize.css"rel="stylesheet"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+/>
+</head>
+<body>
+
+    <header>
+        <nav>
+            <a href="index.php" class="d-none d-sm-block header-left">実は短い！？あなたに残された本当の寿命を診断します。</a>
+            <a href="https://twitter.com/yamato70life" class="header-right start-btn">作った人</a>
+        </nav>
+    </header>
+
+
+    <main>
+        <section class="top-section text-center wow animate__fadeIn" data-wow-duration="3.0s">
+            <img src="https://studio-k-k.com/lifetime/img/logo.png" alt="ロゴです" class="top-logo">
+        </section>
