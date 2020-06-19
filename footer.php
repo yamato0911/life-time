4e7db8de295448c1f437e594b8d6d85fb0219560
yamato0911 <60542163+yamato0911@users.noreply.github.com>
2020-06-19 20:29:03 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,37 @@
+
+            <table class="container table ">
+                <tbody>
+                    <tr>
+                        <th><span class="badge badge-info">Creater</span></th>
+                        <td>Yamato</td>
+                    </tr>
+                    <tr>
+                        <th><span class="badge badge-info">Mail</span></th>
+                        <td>abc-create@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <th><span class="badge badge-info">Web</span></th>
+                        <td>abc-create.com
+                            <button class="table-btn">HP</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><span class="badge badge-info">twitter</span></th>
+                        <td><a href="https://twitter.com/yamato70life">@yamato70life</a><button class="table-btn"><a href="https://twitter.com/yamato70life">twitter</a></button></td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </main>
+        <footer>
+            <p class="footer-text">©️ Create by Yamato All Rights Reserved</p>
+        </footer>
+        <script type="text/javascript" src="main.js"></script>  
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+        <script>
+            new WOW({
+            mobile: false
+            }).init();
+        </script>
+  
+    
